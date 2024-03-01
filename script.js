79d3faabe3f6cdea37adecf1f927059a0a7b153b
vishaljavare4 GitHub <vishaljavare4@gmail.com>
2024-03-01 19:02:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+var color = [
+    "red",
+    "green",
+    "cyan",
+    "black",
+    "voilet",
+    "blue",
+    "yellow",
+    "purple",
+    "orange",
+    "lightgreen",
+    "lemon",
+  ];
+  var index = 0;
+  var isTriangle = false;
+  
+  var cShape = document.getElementById("change-shape");
+  var cColor = document.getElementById("change-color");
+  
+  cShape.addEventListener("click", changeShape);
+  cColor.addEventListener("click", changeColor);
+  
+  function changeColor() {
+  //   console.log(index);
+    if (index === color.length) {
+      index = 0;
+    }
+    document.getElementById("circle").style.backgroundColor = color[index];
+    index++;
+  }
+  
+  function changeShape() {
+    if (!isTriangle) {
+      var i = document.getElementsByClassName("inner")[0];
+      i.className = "triangle-bottom-left";
+      isTriangle = true;
+    }else{
+      var i = document.getElementsByClassName("triangle-bottom-left")[0];
+      i.className = "inner";
+      isTriangle = false;
+    }
+  }
+  
